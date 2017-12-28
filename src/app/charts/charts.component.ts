b94fb41a82c17aab000721be79274e1a45eb901c
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-12-28 08:19:47 -0700

    have pie chart update function

--- src/app/charts/charts.component.ts
+++ src/app/charts/charts.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import * as D3 from "d3";
-//import * as d3pie from 'd3pie';
 import 'd3pie';
 
 @Component({
@@ -11,91 +10,100 @@ import 'd3pie';
 
 export class ChartsComponent implements OnInit {
 
-  public data: any = {};
-  //public d3pie: any;
+  private pieChartData: any = {};
+  private pieChartContent: Array<any>;
 
 
   constructor() { }
 
   ngOnInit() {
-  this.data = {
+    this.initPieChart();
+  }
 
-  "header": {
-    "title": {
-      "text": "JYNX",
-      "fontSize": 24,
-      "font": "open sans"
-    },
-    "subtitle": {
-      "text": "Your cryptocurrency portfolio",
-      "color": "#999999",
-      "fontSize": 12,
-      "font": "open sans"
-    },
-    "titleSubtitlePadding": 9
-  },
-  "footer": {
-    "color": "#999999",
-    "fontSize": 10,
-    "font": "open sans",
-    "location": "bottom-left"
-  },
-  "size": {
-    "canvasWidth": 590,
-    "pieOuterRadius": "90%"
-  },
-  "data": {
-    "sortOrder": "value-desc",
-    "content": [{
+  private initPieChart (): void {
+    this.pieChartContent = [{
       "label": "Select a coin to get started",
       "value": 67706,
       "color": "#CEE9F9"
-    }]
-  },
-  "labels": {
-    "outer": {
-      "pieDistance": 32
-    },
-    "inner": {
-      "hideWhenLessThanPercentage": 3
-    },
-    "mainLabel": {
-      "fontSize": 11
-    },
-    "percentage": {
-      "color": "#ffffff",
-      "decimalPlaces": 0
-    },
-    "value": {
-      "color": "#adadad",
-      "fontSize": 11
-    },
-    "lines": {
-      "enabled": true
-    },
-    "truncation": {
-      "enabled": true
-    }
-  },
-  "effects": {
-    "pullOutSegmentOnClick": {
-      "effect": "linear",
-      "speed": 400,
-      "size": 8
-    }
-  },
-  "misc": {
-    "gradient": {
-      "enabled": true,
-      "percentage": 100
-    }
+    }];
+
+    this.pieChartData = {
+        "header": {
+          "title": {
+            "text": "",
+            "fontSize": 24,
+            "font": "open sans"
+          },
+          "subtitle": {
+            "text": "",
+            "color": "#999999",
+            "fontSize": 12,
+            "font": "open sans"
+          },
+          "titleSubtitlePadding": 9
+        },
+        "footer": {
+          "color": "#999999",
+          "fontSize": 10,
+          "font": "open sans",
+          "location": "bottom-left"
+        },
+        "size": {
+          "canvasWidth": 590,
+          "pieOuterRadius": "90%"
+        },
+        "data": {
+          "sortOrder": "value-desc",
+          "content": this.pieChartContent
+        },
+        "labels": {
+          "outer": {
+            "pieDistance": 32
+          },
+          "inner": {
+            "hideWhenLessThanPercentage": 3
+          },
+          "mainLabel": {
+            "fontSize": 11
+          },
+          "percentage": {
+            "color": "#ffffff",
+            "decimalPlaces": 0
+          },
+          "value": {
+            "color": "#adadad",
+            "fontSize": 11
+          },
+          "lines": {
+            "enabled": true
+          },
+          "truncation": {
+            "enabled": true
+          }
+        },
+        "effects": {
+          "pullOutSegmentOnClick": {
+            "effect": "linear",
+            "speed": 400,
+            "size": 8
+          }
+        },
+        "misc": {
+          "gradient": {
+            "enabled": true,
+            "percentage": 100
+          }
+        }
+      }
+      var pie = new d3pie("myPie", this.pieChartData);
   }
-}
 
-
-
-  var pie = new d3pie("myPie", this.data);
-
-
+  public updatePieChart(newPieChartContent): void {
+    this.pieChartContent = newPieChartContent;
+    var pie = new d3pie("myPie", this.pieChartData);  
   }
+
+  // private createPieChart (pieChartData): void {
+  //
+  // }
 }
