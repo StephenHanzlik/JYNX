cb4efc6bbec7808428d956eecda64742279a8ca3
Stephen Hanzlik <stephenhanzlik@gmail.com>
2017-12-29 15:29:57 -0700

    removed extra folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../Semantic-UI-CSS-master 2/semantic.min.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'JYNX';
