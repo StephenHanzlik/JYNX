7e236734e570fd3badd53603f2bb20f21b31ac3c
Stephen Hanzlik <stephenhanzlik@gmail.com>
2018-01-06 11:54:11 -0700

    added components and routing for log-in sign-up and user-profile

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+   import { RouterModule, Routes } from '@angular/router';
+   import { ChartsComponent } from '../charts/charts.component';
+   import { LogInComponent } from '../log-in/log-in.component';
+   import { SignUpComponent } from '../sign-up/sign-up.component';
+   import { UserProfileComponent } from '../user-profile/user-profile.component';
+
+   const routes: Routes = [
+       {
+           path: 'charts',
+           component: ChartsComponent,
+       },
+       {
+            path: 'log-in',
+            component: LogInComponent,
+       },
+       {
+            path: 'sign-up',
+            component: SignUpComponent,
+       },
+       {
+            path: 'user-profile',
+            component: UserProfileComponent,
+       }
+   ];
+
+   @NgModule({
+       imports: [
+           RouterModule.forRoot(routes)
+       ],
+       exports: [
+           RouterModule
+       ],
+       declarations: []
+   })
+   export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,30 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { SuiModule } from 'ng2-semantic-ui';
 import { HttpModule } from '@angular/http';
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
 import { ChartsComponent } from './charts/charts.component';
 import { CryptoCompareService } from './crypto-compare.service';
-import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChartsComponent,
-    LoginComponent
+    UserProfileComponent,
+    LogInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     SuiModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule,
   ],
   providers: [CryptoCompareService],
   bootstrap: [AppComponent]
