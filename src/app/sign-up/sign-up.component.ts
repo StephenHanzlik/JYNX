1bb5fa2aec3b8e7a734492c447b859a2ee84a7eb
Stephen Hanzlik <stephenhanzlik@gmail.com>
2018-01-11 06:15:11 -0700

    have sign up route logging form data

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators, AbstractControl,  } from '@angular/forms';
+import { Response, Http } from '@angular/http';
 import { TierionService } from '../services/tierion/tierion.service';
 
 @Component({
@@ -15,7 +15,8 @@ export class SignUpComponent implements OnInit {
   password: AbstractControl;
 
   constructor(fb: FormBuilder,
-    private tierionService: TierionService) {
+    private tierionService: TierionService,
+    private http: Http) {
       this.signUpForm = fb.group({
           "userName": ['', Validators.required],
           "email": ['', Validators.required],
@@ -32,7 +33,12 @@ export class SignUpComponent implements OnInit {
   }
 
   public addUser(form: any){
-  //  this.http.post('/api/')
+    console.log("add user Run");
+    //post to our node api
+    this.http.post('/api/sign-up', form).subscribe( res => {
+      console.log("res " + res);
+    })
+    //post directly to tierion DB.   My be deppricated after node route is finsihed
     this.tierionService.signUpUser(form).subscribe( res => {
     })
   }
