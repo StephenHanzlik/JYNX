3efeff46e45dce9c17b1e406ce862d518e9578d9
Stephen Hanzlik <stephenhanzlik@gmail.com>
2018-01-11 06:41:05 -0700

    have DB user added to config string

--- server.js
+++ server.js
@@ -5,8 +5,12 @@ const path = require('path');
 const http = require('http');
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+
 // API file for interacting with MongoDB
 const api = require('./server/routes/api');
+
+let db;
 
 // Parsers
 app.use(bodyParser.json());
@@ -14,6 +18,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Angular DIST output folder
 app.use(express.static(path.join(__dirname, 'dist')));
+
+MongoClient.connect('mongodb://jynx-db-user:y6t5w8M21@ds151207.mlab.com:51207/jynx', (err, database) => {
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
 
 // API location
 app.use('/api', api);
@@ -26,10 +38,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
+
 //Set Port
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-const server = http.createServer(app);
-
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+// const port = process.env.PORT || '3000';
+// app.set('port', port);
+//
+// const server = http.createServer(app);
+//
+// server.listen(port, () => console.log(`Running on localhost:${port}`));
--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const router = express.Router();
 
-const MongoClient = require('mongodb').MongoClient
 
 //const crypto = require('crypto');
 // const sha256 = crypto.createHash('sha256');
@@ -11,9 +10,6 @@ const MongoClient = require('mongodb').MongoClient
 const bcrypt = require('bcrypt');
 //const jwt = require('jsonwebtoken');
 //const privateKey = 'my_awesome_cookie_signing_key';
-MongoClient.connect('mongodb://jynx:y6t5w8M21@ds151207.mlab.com:51207/jynx', (err, database) => {
-  console.log("MongoClient connected!!!");
-})
 
 router.post('/sign-up', function(req, res) {
   console.log("req: " + JSON.stringify(req.body));
