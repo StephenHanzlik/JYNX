9416dbf7df73e6cbdf311b052ed5e1769880195c
Stephen Hanzlik <stephenhanzlik@gmail.com>
2018-02-04 12:40:10 -0700

    deleted charts component and user-profile component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,7 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ChartsComponent } from './charts/charts.component';
 import { CryptoCompareService } from './services/crypto-compare/crypto-compare.service';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { TierionService } from './services/tierion/tierion.service';
@@ -29,8 +27,6 @@ export function highchartsFactory() {
 @NgModule({
   declarations: [
     AppComponent,
-    ChartsComponent,
-    UserProfileComponent,
     LogInComponent,
     SignUpComponent,
     HomeComponent,
