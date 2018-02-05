2bccef1dbb9acce0e0cd14b633b5a03cd7804f3a
Stephen Hanzlik <stephenhanzlik@gmail.com>
2018-02-05 11:03:28 -0700

    styling cards better

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from '../home/home.component';
-import { ChartsComponent } from '../charts/charts.component';
+//import { ChartsComponent } from '../charts/charts.component';
 import { LogInComponent } from '../log-in/log-in.component';
 import { SignUpComponent } from '../sign-up/sign-up.component';
-import { UserProfileComponent } from '../user-profile/user-profile.component';
+//import { UserProfileComponent } from '../user-profile/user-profile.component';
 import { SharablePortfolioComponent } from '../sharable-portfolio/sharable-portfolio.component';
 import { AdminComponent } from '../admin/admin.component';
 
